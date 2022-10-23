724ec36ced6a5c62ded9c0b7883a5a7d4e9fab63
qijiajun <1481700559@qq.com>
2022-10-23 13:36:06 +0800

    add Pinia

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,11 @@ import ElementPlus from 'element-plus'
 import  'element-plus/dist/index.css'
 // 引入Vue Router
 import router from './router';
-
+// 引入pinia
+import pinia from './store';
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
