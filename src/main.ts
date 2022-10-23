2f3a8d7bfad9068ae25d984f7fa8dd80c33d3b3e
qijiajun <1481700559@qq.com>
2022-10-23 13:31:19 +0800

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// 引入tailwindcss
+import '@/assets/styles/tailwind.scss'
 // 引入Element Plus
 import ElementPlus from 'element-plus'
 import  'element-plus/dist/index.css'
 // 引入Vue Router
 import router from './router';
+
 const app = createApp(App)
 
 app.use(ElementPlus)
