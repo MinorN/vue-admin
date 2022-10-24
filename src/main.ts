047610d9cf574faf72b26f82905221f2b4a1b1fd
qijiajun <1481700559@qq.com>
2022-10-24 16:49:40 +0800

    add mock

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import '@/assets/styles/tailwind.scss'
 // 引入Element Plus
 import ElementPlus from 'element-plus'
 import  'element-plus/dist/index.css'
+// 引入Element Plus Icon
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 引入Vue Router
 import router from './router';
 // 引入pinia
@@ -13,6 +15,9 @@ import pinia from './store';
 const app = createApp(App)
 
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(router)
 app.use(pinia)
 app.mount('#app')
