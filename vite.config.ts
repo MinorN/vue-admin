5815e31f59c47c5b6f4e273d8058c29218ccc276
qijiajun <1481700559@qq.com>
2022-10-24 16:59:06 +0800

    add axios

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-import path from 'node:path'
-
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
+import {viteMockServe} from 'vite-plugin-mock';
 // 全局引入ref...等
-import AutoImport from 'unplugin-auto-import/vite'
-export default defineConfig({
-  plugins: [
-      AutoImport({
-        imports:['vue','vue-router','pinia'],
-        dts:'./src/auto-import.d.ts'
-      }),
-      vue()
-  ],
-  resolve:{
-    alias: {
-      '@' : path.resolve(__dirname,'src')
-    }
-  }
-})
+import AutoImport from 'unplugin-auto-import/vite';
+
+
+export default ({command}) => {
+    return defineConfig({
+        plugins: [
+            AutoImport({
+                imports: ['vue', 'vue-router', 'pinia'],
+                dts: './src/auto-import.d.ts'
+            }),
+            vue(),
+            viteMockServe({
+                // default
+                mockPath: 'mock',
+                localEnabled: command === 'serve',
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src')
+            }
+        }
+    });
+}
